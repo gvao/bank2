82eed0f534e018a6faa102484ea91b71a854b9ed
Yuri Galvao <yuri.gvao@gmail.com>
2025-08-24 02:24:26 -0300

    created Layout to private routes

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,23 @@
+import { AppSidebar } from "@/src/components/dashboard/app-sidebar"
+import { SiteHeader } from "@/src/components/dashboard/site-header"
+import { SidebarProvider, SidebarInset } from "@/src/components/ui/sidebar"
+
+const Layout = ({ children }: { children: React.ReactNode }) => (
+    <SidebarProvider
+        style={
+            {
+                "--sidebar-width": "calc(var(--spacing) * 72)",
+                "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+        }
+    >
+        <AppSidebar variant="inset" />
+
+        <SidebarInset>
+            {children}
+        </SidebarInset>
+
+    </SidebarProvider>
+)
+
+export default Layout
--- src/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-import { DashboardComponent } from "@/src/components/dashboard-component";
-
-export default function Page() {
-  return (
-    <DashboardComponent />
-  )
-}
-
